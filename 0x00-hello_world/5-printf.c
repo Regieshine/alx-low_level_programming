fe10511997ea408e0265a5fbd7693dae49f4f11b
Regieshine <reginaotoro@gmail.com>
2023-09-07 09:42:40 -0700

    betty

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - With proper grammar, but the outcome is a piece of art.
+ * Return: 0 (great)
+ */
+int main(void)      
+{       
+printf("with proper grammer, but the outcome is a piece of art,\n")
+return (0);
+}       
